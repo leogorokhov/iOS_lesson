f0a5cbe2f8e868bad839b96711753d2c8d1c876c
Leo <lenia.goroxow@yandex.ru>
2023-10-09 20:34:02 +0300

    deleted storyboard

--- iOS_lesson/SceneDelegate.swift
+++ iOS_lesson/SceneDelegate.swift
@@ -13,10 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { 
+            return } // создаем сцену
+      
+        let window = UIWindow(windowScene: windowScene) // создаем окно
+        let viewController = ViewController() // создаем контроллер
+        window.rootViewController = viewController // делаем его главным
+        window.makeKeyAndVisible() // делаем его ключевым и видимым
+        self.window = window // говорим, что это тот самый window вне функции
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
